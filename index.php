89c5944800d31ed6ec669ab00028ddcf9e7095d3
Chloe0916 <chloe.nolimit@gmail.com>
2021-09-03 20:14:33 +0900

    1번 작업물입니다

---
+++ index.php
@@ -0,0 +1,309 @@
+<!DOCTYPE HTML>
+<!--
+	Helios by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>Helios by HTML5 UP</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+	</head>
+	<body class="homepage is-preload">
+		<div id="page-wrapper">
+
+			<!-- Header -->
+				<div id="header">
+
+					<!-- Inner -->
+						<div class="inner">
+							<header>
+								<h1><a href="index.html" id="logo">I'm Chloe</a></h1>
+								<hr />
+								<p>Do you want to know me?</p>
+							</header>
+							<footer>
+								<a href="#banner" class="button circled scrolly">Knock</a>
+							</footer>
+						</div>
+
+					<!-- Nav -->
+						<nav id="nav">
+							<ul>
+								<li><a href="index.html">Home</a></li>
+								<li>
+									<a href="#">Dropdown</a>
+									<ul>
+										<li><a href="#">Lorem ipsum dolor</a></li>
+										<li><a href="#">Magna phasellus</a></li>
+										<li><a href="#">Etiam dolore nisl</a></li>
+										<li>
+											<a href="#">And a submenu &hellip;</a>
+											<ul>
+												<li><a href="#">Lorem ipsum dolor</a></li>
+												<li><a href="#">Phasellus consequat</a></li>
+												<li><a href="#">Magna phasellus</a></li>
+												<li><a href="#">Etiam dolore nisl</a></li>
+											</ul>
+										</li>
+										<li><a href="#">Veroeros feugiat</a></li>
+									</ul>
+								</li>
+								<li><a href="left-sidebar.html">Left Sidebar</a></li>
+								<li><a href="right-sidebar.html">Right Sidebar</a></li>
+								<li><a href="no-sidebar.html">No Sidebar</a></li>
+							</ul>
+						</nav>
+
+				</div>
+
+			<!-- Banner -->
+				<section id="banner">
+					<header>
+						<h2>Hello. My name is <strong>Chloe</strong>.</h2>
+						<p>
+							I'm working as a  <strong>Creative</strong> </a>
+							in <a href="http://apple.com/kr">Apple Yeouido Store.
+						</p>
+					</p></p></p>
+						<a href="#" class="vedio featured">
+							<video src="./images/movie.MOV"
+		                    width="800px"
+	                     	height="500px"
+	                     	controls
+	                    	autoplay
+		
+	                        ></video>
+
+	</header>
+				</section>
+
+        
+						
+			<!-- Carousel -->
+				<section class="carousel">
+					<div class="reel">
+
+						
+
+						<article>
+							<a href="#" class="image featured"><img src="images/01.JPG" style="max-width: 100%; height: 100%" ;alt="" /></a>
+							<header>
+								<h3><a href="#">Shall we dive in togetehr?</a></h3>
+					
+						</article>
+
+						<article>
+							<a href="#" class="image featured"><img src="images/02.jpg" style="max-width: 100%; height: 100%x" ;alt="" /></a>
+							<header>
+								<h3><a href="#">Everybody can be a Model.</a></h3>
+						
+						</article>
+
+						<article>
+							<a href="#" class="image featured"><img src="images/03.jpg" style="max-width: 100%; height: 100%" ;alt="" /></a>
+							<header>
+								<h3><a href="#">Sphero is my friend.</a></h3>
+							
+						</article>
+
+						<article>
+							<a href="#" class="image featured"><img src="images/04.heic" style="max-width: 100%; height: 100%" ;alt="" /></a>
+							<header>
+								<h3><a href="#">Where everyone is welcome </a></h3>
+						
+						</article>
+
+						<article>
+							<a href="#" class="image featured"><img src="images/05.jpg" style="max-width: 100%; height: 100%" ;alt="" /></a>
+							<header>
+								<h3><a href="#">Inspire customer's creativity </a></h3>
+					
+						</article>
+
+						<article>
+							<a href="#" class="image featured"><img src="images/06.JPG" style="max-width: 100%; height: 100%" ;alt="" /></a>
+							<header>
+								<h3><a href="#">That is Apple's Ecosystem. </a></h3>
+						
+						</article>
+
+						<article>
+							<a href="#" class="image featured"><img src="images/07.jpg" style="max-width: 100%; height: 100%" ;alt="" /></a>
+							<header>
+								<h3><a href="#">Do you mind if I win? </a></h3>
+				
+						</article>
+
+						<article>
+							<a href="#" class="image featured"><img src="images/08.jpg" style="max-width: 100%; height: 100%" ;alt="" /></a>
+							<header>
+								<h3><a href="#"> Welcome to Today at Apple! </a></h3>
+				
+						</article>
+
+						<article>
+							<a href="#" class="image featured"><img src="images/09.jpg" style="max-width: 100%; height: 100%" ;alt="" /></a>
+							<header>
+								<h3><a href="#">You can keep my signature:) </a></h3>
+					
+						</article>
+
+						<article>
+							<a href="#" class="image featured"><img src="images/10.jpg" style="max-width: 100%; height: 100%" ; alt="" /></a>
+							<header>
+								<h3><a href="#">We are connected! </a></h3>
+					
+						</article>
+						
+						<article>
+							<a href="#" class="image featured"><img src="images/11.png" style="max-width: 100%; height: 100%" ;alt="" /></a>
+							<header>
+								<h3><a href="#">[NPI] What a exiciting! </a></h3>
+			
+						</article>
+
+						</article>
+
+						<article>
+							<a href="#" class="image featured"><img src="images/13.gif" style="max-width: 100%; height: 250px" ; alt="" /></a>
+							<header>
+								<h3><a href="#">Give me all, please..</a></h3>
+						
+						</article>
+
+						<article>
+							<a href="#" class="image featured"><img src="images/14.jpg" style="max-width: 100%; height: 250px" ; alt="" /></a>
+							<header>
+								<h3><a href="#">I love my Airtag! </a></h3>
+				
+						</article>
+					</div>
+				</section>
+
+			<!-- Main -->
+			<p><p></p>
+				<div class="wrapper style2">
+
+					<article id="main" class="container special">
+						<a href="#" class="image featured"><img src="images/pic01.jpeg" alt="" /></a>
+						<header>
+							<h2><a>Draw ourselves in Peanuts Character</a></h2>
+							<p>
+								
+                                I have worked inspire others and create a collaborative atmosphere. 
+								It was able to unlocking my colleagues creativity and these experiences could inspire customers on the floor. 
+								In a short time, I completed with 100% participation with all team members. 
+				
+							</p>
+						</p>
+						<footer>
+							<a href="#" class="button">Watch Video on Loop</a>
+						</br>
+							&emsp; 
+
+
+							<p>
+							<div class="wrapper style2">
+			
+								<article id="main" class="container special">
+									<a href="#" class="image featured"><img src="images/pic02.png" alt="" /></a>
+									<header>
+										<h2><a>Grand opening of Apple Yeouido </a></h2>
+										<p>	
+											To celebrate and to give our customer exceptional memory in our store, </br> the Forum team had been taking customer's picture with Apple Yeouido logo.
+										</p>
+							</p>		
+						</footer>
+					</article>
+				</div>
+
+			<!-- Features -->
+				<div class="wrapper style1">
+
+					<section id="features" class="container special">
+						<header>
+							<h2>Taking Picture can capture the moment. </h2>
+							<p>Especillay, I love taking picture of nature with my iPhone.</br> 
+								Do I love travelling? More than anyone! </p>
+						</header>
+						<div class="row">
+							<article class="col-4 col-12-mobile special">
+								<a href="#" class="image featured"><img src="images/pic03.JPG" alt="" /></a>
+							
+								</p>
+							</article>
+							<article class="col-4 col-12-mobile special">
+								<a href="#" class="image featured"><img src="images/pic04.JPG" alt="" /></a>
+							
+								</p>
+							</article>
+							<article class="col-4 col-12-mobile special">
+								<a href="#" class="image featured"><img src="images/pic05.JPG" alt="" /></a>
+
+							</p>
+						</article>
+						<article class="col-4 col-12-mobile special">
+							<a href="#" class="image featured"><img src="images/pic06.JPG" alt="" /></a>
+								
+						</p>
+					</article>
+					<article class="col-4 col-12-mobile special">
+						<a href="#" class="image featured"><img src="images/pic07.JPG" alt="" /></a>
+							
+					</p>
+				</article>
+				<article class="col-4 col-12-mobile special">
+					<a href="#" class="image featured"><img src="images/pic08.jpeg" style="max-width: 100%; height: 280px" ;alt="" /></a>
+						
+								</p>
+							</article>
+						</div>
+					</section>
+
+				</div>
+
+			<!-- Footer -->
+				<div id="footer">
+					<div class="container">
+			
+						<div class="row">
+							<div class="col-12">
+
+								<!-- Contact -->
+									<section class="contact">
+										<header>
+											<h3>2021 &copy; You can send me an <a href="mailto: jinjoo_jung@apple.com" title="E-mail">E-mail</h3>
+							
+									</section>
+
+								<!-- Copyright -->
+									<div class="copyright">
+										<ul class="menu">
+											<li>&copy; Untitled. All rights reserved.</li>
+										</ul>
+									</div>
+
+							</div>
+
+						</div>
+					</div>
+				</div>
+
+		</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.dropotron.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
